beb74bb8431e4b36a651041977ef3c05dfd0b209
Conor McGrory <cpmcgrory@gmail.com>
2020-12-11 01:58:10 -0500

    Got echo filter working in Julia, but way too slow

--- basic_prototype/julia/realtime.jl
+++ basic_prototype/julia/realtime.jl
@@ -11,6 +11,36 @@ const N_PTS = 1000
 const N_NEURONS = 698
 
 
+function hdr_encode(n_neurons::Integer)
+    return reinterpret(UInt8, UInt16[convert(UInt16, n_neurons)])
+end
+
+
+function hdr_decode(hdr_bytes::Array{UInt8})
+    return reinterpret(UInt16, hdr_bytes)[1]
+end
+
+
+function spks_encode(spks::Array{Int8})
+    return reinterpret(UInt8, spks)
+end
+
+
+function spks_decode(spk_bytes::Array{UInt8})
+    return reinterpret(Int8, spk_bytes)
+end
+
+
+function fpred_encode(fpred::Array{Float64})
+    return reshape(reinterpret(UInt8, fpred), :, 1)
+end
+
+
+function fpred_decode(fpred_bytes::Array{UInt8})
+    return reinterpret(Float64, reshape(fpred_bytes, 8, :))
+end
+
+
 function probe_mode(host::IPAddr, port::Integer, in_fpath::String, out_fpath::String)
 
     # Load test signal from file (need to swap axes; signal stored by NumPy)
@@ -19,9 +49,9 @@ function probe_mode(host::IPAddr, port::Integer, in_fpath::String, out_fpath::St
         permutedims(read(file, "spks"), [2, 1])
     end
     println("Done.")
-
+    
     # Number of neurons (must be less than 65535)
-    n_neurons = convert(UInt16, size(spks, 1))
+    n_neurons = size(spks, 1)
 
     println("Connecting to $host:$port...")
     conn = connect(host, port)
@@ -30,7 +60,7 @@ function probe_mode(host::IPAddr, port::Integer, in_fpath::String, out_fpath::St
     println("Sending header information...")
 
     # Send header
-    hdr_bytes = reinterpret(UInt8, UInt16[n_neurons])
+    hdr_bytes = hdr_encode(n_neurons)
     write(conn, hdr_bytes)
     
     # Receive ACK byte
@@ -43,7 +73,7 @@ function probe_mode(host::IPAddr, port::Integer, in_fpath::String, out_fpath::St
 
     println("Sending signal...")
 
-    filter_preds = fill(NaN, N_PTS)
+    filter_preds = fill(NaN, n_neurons, N_PTS)
     rt_times_us = fill(NaN, N_PTS)
 
     for i in 1:N_PTS
@@ -51,10 +81,10 @@ function probe_mode(host::IPAddr, port::Integer, in_fpath::String, out_fpath::St
         t_start_ns = time_ns()
 
         # Write spike counts (just neuron 1)
-        write(conn, spks[1, i])
+        write(conn, spks_encode(spks[:, i]))
 
-        # Read filter value from client
-        filter_preds[i] = read(conn, UInt8)
+        # Read filter value from client (8 bytes per neuron)
+        filter_preds[:, i] = fpred_decode(read(conn, n_neurons * 8))
 
         # Compute round-trip latency (microseconds)
         rt_times_us[i] = (time_ns() - t_start_ns) / 1000
@@ -96,7 +126,8 @@ function processor_mode(host::IPAddr, port::Integer)
     println("Receiving header information...")
 
     # Receive header
-    n_neurons = read(conn, UInt16)
+    hdr_bytes = read(conn, 2)
+    n_neurons = hdr_decode(hdr_bytes)
     
     # Send ACK
     write(conn, ACK_BYTE)
@@ -105,10 +136,17 @@ function processor_mode(host::IPAddr, port::Integer)
 
     println("Filtering signal...")
 
-    # Run simple "echo filter" on signal
     while !eof(conn)
-        spks = read(conn, UInt8)
-        write(conn, spks)
+
+        # Receive spikes from probe (1 byte per neuron)
+        spks = spks_decode(read(conn, n_neurons))
+
+        # "Echo filter"
+        fpred = convert(Array{Float64}, spks)
+
+        # Send filter prediction to probe
+        write(conn, fpred_encode(fpred))
+
     end
 
     println("Done.")
